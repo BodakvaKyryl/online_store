982dbe3eaff71b5f619afdce73cc02e24a62099d
Bodakva Kyryl <bodakvakyryl@gmail.com>
2023-11-27 11:06:25 +0200

    completed class ApiError and created controllers

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,9 @@
 const Router = require('express').Router
 const router = new Router()
+const userController = require('../controllers/userController')
 
-router.post('/registration')
-router.post('/login')
-router.get('/auth', (req, res) => {
-	res.json({ message: 'user auth router is working' })
-})
+router.post('/registration', userController.registration)
+router.post('/login', userController.login)
+router.get('/auth', userController.auth)
 
 module.exports = router
